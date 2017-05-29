df4b8422ba6130839865d46d434584ddab092a1b
Piotr Zawislak <running.t@gmail.com>
2017-05-29 23:11:03 +0200

    First real unit tests and some fixes found

--- Enhancer.py
+++ Enhancer.py
@@ -2,6 +2,8 @@ from xml.etree import ElementTree
 import requests
 import urllib
 import json
+import time
+from collections import OrderedDict
 
 class Enhancer(object):
     namespaces = {'tcx':'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
@@ -9,12 +11,14 @@ class Enhancer(object):
                   '':'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'}
     API_KEY='XXXXXX'
     API_URL='http://elevation.mapzen.com/height'
+    CHUNK_SIZE = 112
 
-    def __init__(self,  input, output, apk='XXXXXX'):
+    def __init__(self,  input, output, api_key=API_KEY, chunk_size = CHUNK_SIZE):
         self.input = input
         self.output = output
-        self.points = {}
-        self.api_key = apk
+        self.points = OrderedDict()
+        self.api_key = api_key
+        self.chunk_size = chunk_size
         for (k,v) in self.namespaces.items():
             ElementTree.register_namespace(k,v)
 
@@ -22,12 +26,14 @@ class Enhancer(object):
         self.etree = ElementTree.parse(self.input)
         self.xml_points = self.etree.findall('.//tcx:Trackpoint', self.namespaces)
 
-    def get_all_points(self):
+    def get_all_points(self, max_points=0):
         for p in self.xml_points:
             longitude = p.find('./tcx:Position/tcx:LongitudeDegrees', self.namespaces)
             latitude = p.find('./tcx:Position/tcx:LatitudeDegrees', self.namespaces)
             if longitude is not None and latitude is not None:
                 self.points[(longitude.text, latitude.text)] = None
+            if max_points and max_points <= len(self.points):
+                self.points = OrderedDict((k, self.points[k]) for k in list(self.points.keys())[0:max_points])
 
     def _chunks(self, _list, n):
         for i in range(0, len(_list), n):
@@ -35,32 +41,46 @@ class Enhancer(object):
 
     def _build_request_urls(self):
         shape_list = [{"lat": k[1], "lon": k[0]} for k in self.points.keys()]
-        for l in self._chunks(shape_list, 128):
+        for l in self._chunks(shape_list, self.chunk_size):
             dic = {'shape': l}
             params=urllib.parse.urlencode({'json':json.dumps(dic), 'api_key':self.api_key})
             yield '{}?{}'.format(self.API_URL,params)
 
-    def get_altitudes(self):
+
+    def _get_responses(self):
+        self.responses = []
         for url in self._build_request_urls():
             try:
                 resp = requests.get(url)
-                json = resp.json
-            except:
-                json = {}
-            else:
-                shape = json.get('shape')
-                height = json.get('height')
+                self.responses.append(resp)
+                if resp.status_code == 429:
+                    time.sleep(2)
+                    resp = requests.get(url)
+                yield resp
+            except Exception as e:
+                print(str(e))
+            time.sleep(0.5)
+
+    def get_altitudes(self):
+        for resp in self._get_responses():
+            if resp.ok:
+                jsn = resp.json()
+                shape = jsn.get('shape')
+                height = jsn.get('height')
                 if shape and height:
-                    res = zip([(x.get('lon'), x.get('lat')) for x in shape], height)
-                    for point,height in res:
-                        self.points[res[point]] = res[height]
+                    shape_list =[(x.get('lon'), x.get('lat')) for x in shape]
+                    res = zip(shape_list, height)
+                    for p,h in res:
+                        self.points[p] = h
 
     def append_altitudes(self):
+        prev = '0'
         for p in self.xml_points:
-            altitude = ElementTree.Etree('AltitudeMeters')
+            altitude = ElementTree.Element('AltitudeMeters')
             longitude = p.find('./tcx:Position/tcx:LongitudeDegrees', self.namespaces)
             latitude = p.find('./tcx:Position/tcx:LatitudeDegrees', self.namespaces)
-            altitude.text =self.points[longitude.text, latitude.text]
+            altitude.text =str(self.points[(longitude.text, latitude.text)] or prev)
+            prev = altitude.text
             p.append(altitude)
 
     def write(self):
--- test/test_Enhancer.py
+++ test/test_Enhancer.py
@@ -1,6 +1,7 @@
 import pytest
-import mock
+from unittest.mock import Mock, patch, call
 from Enhancer import Enhancer
+from xml.etree import ElementTree
 
 class TestEnhancer(object):
 
@@ -20,7 +21,60 @@ class TestEnhancer(object):
     def enhancer(self, test_input, test_output, test_key):
         return Enhancer(test_input, test_output, test_key)
 
+
     def test_constructor(self, enhancer, test_input, test_output, test_key):
         assert enhancer.input == test_input
         assert enhancer.output == test_output
         assert enhancer.api_key == test_key
+        assert enhancer.chunk_size == Enhancer.CHUNK_SIZE
+
+    @pytest.fixture
+    def points(self):
+        points = []
+        for x in range(5):
+            point = Mock(spec=ElementTree.Element)
+            text = Mock()
+            text.text = str(x)
+            point.find = Mock(return_value=text)
+            points.append(point)
+        return points
+
+    @pytest.fixture
+    def mock_etree(self, points):
+        etree = Mock()
+        etree.findall = Mock(return_value=points)
+        return etree
+
+    def test_parse_xml(self, enhancer, points, mock_etree):
+        with patch.object(ElementTree, 'parse', return_value = mock_etree) as parse_mock:
+            enhancer.parse_xml()
+            assert enhancer.xml_points == points
+            parse_mock.assert_called_once_with(enhancer.input)
+            mock_etree.findall.assert_called_once_with('.//tcx:Trackpoint', enhancer.namespaces)
+
+    @pytest.mark.parametrize('limit', (0, None))
+    def test_get_all_points(self, limit, enhancer, points):
+        enhancer.xml_points = points
+        if limit is None:
+            enhancer.get_all_points()
+        else:
+            enhancer.get_all_points(limit)
+        assert enhancer.points =={(str(x),str(x)):None  for x in range(5)}
+        for p in points:
+            assert p.find.call_count == 2
+            assert p.find.call_args_list == \
+                [call('./tcx:Position/tcx:LongitudeDegrees', enhancer.namespaces),
+                 call('./tcx:Position/tcx:LatitudeDegrees', enhancer.namespaces)]
+
+    @pytest.mark.parametrize('limit', (1,4,5,6))
+    def test_get_all_points_limited(self, limit, enhancer, points):
+        enhancer.xml_points = points
+        enhancer.get_all_points(limit)
+        print(enhancer.points)
+        assert enhancer.points =={(str(x),str(x)):None  for x in range(min(5,limit))}
+        for p in points[0:min(5,limit)]:
+            assert p.find.call_count == 2
+            assert p.find.call_args_list == \
+                [call('./tcx:Position/tcx:LongitudeDegrees', enhancer.namespaces),
+                    call('./tcx:Position/tcx:LatitudeDegrees', enhancer.namespaces)]
+
